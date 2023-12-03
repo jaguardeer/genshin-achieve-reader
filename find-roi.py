319bb4aa5536864877acfe667a784d11e9b506e8
jaguardeer <pdeloz@gmail.com>
2023-12-03 12:06:51 -0600

    playing with HSV

--- find-roi.py
+++ find-roi.py
@@ -44,20 +44,7 @@ def on_trackbar(val):
 	# draw contours
 	new_frame = frame.copy()
 	cv.drawContours(new_frame, new_contours, -1, (0,255,0), 1)
-	#new_frame = cv.resize(new_frame, dsize=(1280,720))
 	cv.imshow(window_title, new_frame)
-	#plt.show()
-
-
-#bin2_trackbar = 'bin2'
-#def on_trackbar_bin(val):
-	#bin_thresh = cv.getTrackbarPos(bin2_trackbar, window_title)
-	#_, img_thresh = cv.threshold(gray, bin_thresh, bin_trackbar_max, cv.THRESH_BINARY)
-	#cv.imshow(window_title, cv.resize(img_thresh, dsize=(1280,720)))
-#cv.namedWindow(window_title)
-#cv.createTrackbar(bin2_trackbar, window_title, 0, 255, on_trackbar_bin)
-#on_trackbar_bin(0)
-#cv.waitKey()
 
 cv.namedWindow(window_title)
 cv.createTrackbar(bin_trackbar, window_title, 225, bin_trackbar_max, on_trackbar)
